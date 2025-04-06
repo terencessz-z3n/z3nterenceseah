6fe19d8c626f3a519daa927b2bb9cba6cff97c27
Terence Seah <z3nterenceseah@gmail.com>
2025-04-06 22:57:58 +0800

    Update env file

--- controllers/xaas.controller.js
+++ controllers/xaas.controller.js
@@ -4,32 +4,6 @@ const uuid = require('uuid');
 const jwtSharedKey = process.env.JWTSHAREDKEY;
 const zendeskURL = process.env.ZENDESKURL
 
-exports.getQualtricsSurvey = async (req, res) => {
-    console.log(req.params.order_id)
-    let order_id = req.params.order_id;
-
-    if (order_id === "123") {
-        console.log("Order ID: " + order_id)
-        res.send({
-            q_name: "",
-            q_email: ""
-        })
-    } else if (order_id === "456") {
-        console.log("Order ID: " + order_id)
-        res.send({
-            q_name: "Aablo",
-            q_email: "aablo@email.com"
-        })
-    }
-};
-
-exports.updateTopDeskTicket = async (req, res) => {
-    console.log("ID: " + req.params.id)
-    console.log("Body: " + req.body);
-
-    // Implement TOPdesk update incident by passing in the req.params.id which represent the custom field that holds the incident ID
-}
-
 exports.getSampleExternalData = async (req, res) => {
     const email = req.body.email;
     console.log(email)
--- services/jwt.services.js
+++ services/jwt.services.js
@@ -6,22 +6,6 @@ const messageKeyId = process.env.MESSAGEKEYID;
 const suncoSecret = process.env.SUNCOSECRET;
 const suncoKeyId = process.env.SUNCOKEYID;
 
-//Typeform
-//Prod
-//const messageSecret = "q_U2vQcY0xwU9bmLKpJQOoUW00x8RNYf2zUcl_MixWzgAh8eSkJYgqIHULPE1BKaot0CCwCvCCsKFgnk5wvBeA";
-//const messageKeyId = "app_66f4b0215444c494d5e1cabb";
-//Sandbox
-//const messageSecret = "2aASvgv4UhVFqjsdRsNLGSEig-IcCUE4U4-VoM6VLYj5hSfCwQH6kPlb97Oz_qIiubOY1fhXiuxFyMgqD1QXfg";
-//const messageKeyId = "app_66ceeb4ebaba0531e758ce27";
-
-//SuperBet
-//const messageSecret = "Gh3WLPFXCjPhsK0mazvwSMS3SwwNUSECmo9hBarMiyqvehkyxQfyAMaFmRu1eBE2mO399pMnxaLbHnt54meXPg";
-//const messageKeyId = "app_6791ea8a5b52c03c2d1857b0";
-
-//ADEO
-//const messageSecret = "-k_RuoId9driqqJz2_XE49JEjMy-jQHKq9n7UaWWqd-Yqx--wiw_uWRvzFhO5ORHyOqCnjYVyKSj-dEOq1R3QQ"
-//const messageKeyId = "app_67a95d55a30b40d2ae2949f5"
-
 //Generate JWT Token
 const generateToken = async (type, req) => {
     switch (type) {
@@ -58,7 +42,6 @@ const generateToken = async (type, req) => {
 const verifyHCToken = async (token) => {
     const jwksClientInstance = jwksClient({
         jwksUri: 'https://z3ntscap.zendesk.com/api/v2/help_center/integration/keys.json'
-        //jwksUri: 'https://adeomarketplace1706263233.zendesk.com/api/v2/help_center/integration/keys.json'
     });
 
     const decodedToken = jwt.decode(token, { complete: true });
